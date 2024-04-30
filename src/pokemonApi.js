9f660037e35a99b8ea2b06f1eeb06e81ea19c919
Calfura <travis_wilson999@hotmail.com>
2024-04-30 17:13:20 +1000

    feat: types and names display

--- src/pokemonApi.js
+++ src/pokemonApi.js
@@ -16,11 +16,26 @@ async function getPokemonData(){
     // result = promiseRepsonce;
 
     return result;
-};
+}
 
 async function putDataOnPage(dataToDisplay){
     document.getElementsByClassName("pokemonName")[0].textContent = dataToDisplay.name;
-};
+    
+    let type1Display = document.getElementsByClassName("pokemonType1")[0];
+    let type2Display = document.getElementsByClassName("pokemonType2")[0];
+
+     type1Display.textContent = "Type 1: " + dataToDisplay.types[0].type.name;
+    //  type1Display.textContent = data.types[0]["type"]["name"];
+
+    if (dataToDisplay.types[1]){
+        // if the data includes a 2nd type, set that as well
+        type2Display.textContent = "Type 2: " + dataToDisplay.types[1].type.name;
+    } else {
+        // if no 2nd type exsits, reset the content in type2Display
+        type2Display.textContent = "Type 2: ";
+    }
+
+}
 
 // Button calls this
 async function getAndDisplayPokemonData(){
@@ -28,7 +43,7 @@ async function getAndDisplayPokemonData(){
     console.log(data)
 
     putDataOnPage(data);
-};
+}
 
 document.getElementById("create-encounter").addEventListener("click", getAndDisplayPokemonData);
 
